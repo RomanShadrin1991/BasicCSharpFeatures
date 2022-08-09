38d16458454bd4bd5e80c977abe9a0d58944d08d
Roman Shadrin <roma.shadrin.1991@gmail.com>
2022-08-09 13:26:38 +0300

    Добавьте файлы проекта.

---
+++ BasicCSharpFeatures/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BasicCSharpFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание #1
+            Console.Write("Введите Ф.И.О: ");
+            string fullName = Console.ReadLine();
+            byte age;
+            checked
+            {
+                Console.Write("Введите возраст: ");
+                age = Convert.ToByte(Console.ReadLine());
+            }
+            Console.Write("Введите e-mail: ");
+            string email = Console.ReadLine();
+            Console.Write("Введите баллы по программированию: ");
+            float programmingPoints = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите баллы по математике: ");
+            float mathPoints = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите баллы по физике: ");
+            float physicsPoints = Convert.ToSingle(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Информация о студенте: \n");
+            var student = new User(fullName, age, email, programmingPoints, mathPoints, physicsPoints);
+            Console.WriteLine(student);
+            Console.ReadKey();
+
+            // Задание #2
+            float score = Sum(programmingPoints, mathPoints, physicsPoints);
+            float GPA = score / 3;
+            Console.WriteLine($"\nОбщая сумма баллов по предметам: {score:0.00}\nСредний балл: {GPA:0.00}");
+        }
+
+        static float Sum(float floatNumber1, float floatNumber2, float floatNumber3)
+        {
+            return floatNumber1 + floatNumber2 + floatNumber3;
+        }
+    }
+}
---
+++ BasicCSharpFeatures/User.cs
@@ -0,0 +1,59 @@
+﻿namespace BasicCSharpFeatures
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Ф.И.О
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public byte Age { get; set; }
+        /// <summary>
+        /// E-mail
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Баллы по программированию
+        /// </summary>
+        public float ProgrammingPoints { get; set; }
+        /// <summary>
+        /// Баллы по математике
+        /// </summary>
+        public float MathPoints { get; set; }
+        /// <summary>
+        /// Баллы по физике
+        /// </summary>
+        public float PhysicsPoints { get; set; }
+        /// <summary>
+        /// Создание нового пользователя
+        /// </summary>
+        /// <param name="fullName"> Ф.И.О </param>
+        /// <param name="age"> Возраст </param>
+        /// <param name="email"> e-mail </param>
+        /// <param name="programmingPoints"> Баллы по программированию </param>
+        /// <param name="mathPoints"> Баллы по математике </param>
+        /// <param name="physicsPoints"> Баллы по физике </param>
+        public User(string fullName, byte age, string email, float programmingPoints, float mathPoints, float physicsPoints)
+        {
+            FullName = fullName;
+            Age = age;
+            Email = email;
+            ProgrammingPoints = programmingPoints;
+            MathPoints = mathPoints;
+            PhysicsPoints = physicsPoints;
+        }
+        public override string ToString()
+        {
+            return $"Ф.И.О: {FullName}" +
+                   $"\nВозраст: {Age}" +
+                   $"\n\tБаллы по программированию: {ProgrammingPoints}" +
+                   $"\n\tБаллы по математике: {MathPoints}" +
+                   $"\n\tБаллы по физике: {PhysicsPoints}";
+        }
+    }
+}
